f997080190ded731963d388e8480b24c8a9d5d6d
Raman <a.ramanola197@gmail.com>
2026-06-22 08:22:27 +0100

    update authentication and email

--- utils/email.js
+++ utils/email.js
@@ -3,9 +3,9 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
 
-  port: Number(process.env.SMTP_PORT) || 465,
+  port: Number(process.env.SMTP_PORT) || 587,
 
-  secure: true,
+  secure: false,
 
   auth: {
     user: process.env.SMTP_USER,
